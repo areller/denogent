9fe150bd490ceb182056a25c5b651ee273db88e4
areller <areller.gm@gmail.com>
2020-10-03 16:12:13 -0400

    remove test tasks

--- build/build.ts
+++ build/build.ts
@@ -5,24 +5,7 @@ import { DenoPermissions } from "../lib/deno/args.ts";
 import deno from "../lib/deno/deno.ts";
 import { delay } from 'https://deno.land/std/async/delay.ts';
 
-const a = task('a')
-    .does(async ctx => {
-        for (let i = 0; i < 10; i++) {
-            ctx?.logger.info('{a} ' + i);
-            await delay(1000);
-        }
-    });
-
-const b = task('b')
-    .does(async ctx => {
-        for (let i = 0; i < 10; i++) {
-            ctx?.logger.info('{b} ' + i);
-            await delay(1000);
-        }
-    });
-
 const test = task('test')
-    .dependsOn([a, b])
     .does(async ctx => {
         await deno.test({
             logger: ctx?.logger,
