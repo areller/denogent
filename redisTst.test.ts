2fba1550cc90d2c2273c051808fee168b3d19014
areller <areller.gm@gmail.com>
2020-10-04 17:32:20 -0400

    correct redis address

--- redisTst.test.ts
+++ redisTst.test.ts
@@ -1,7 +1,7 @@
 import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.72.0/testing/asserts.ts";
 const redis = await connect({
-  hostname: "127.0.0.1",
+  hostname: "redis",
   port: 6379
 });
 
