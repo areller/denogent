79eae946c44c9e052542d3aa6587654102e33bc4
areller <areller.gm@gmail.com>
2020-10-04 17:30:11 -0400

    redis test

---
+++ redisTst.test.ts
@@ -0,0 +1,11 @@
+import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.72.0/testing/asserts.ts";
+const redis = await connect({
+  hostname: "127.0.0.1",
+  port: 6379
+});
+
+Deno.test('redis', async () => {
+    await redis.set('name', 'areller');
+    assertEquals(await redis.get('name'), 'areller');
+});
