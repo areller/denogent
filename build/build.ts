53fba930804223b54b5f919f2297a0c6eb405e03
areller <areller.gm@gmail.com>
2020-10-07 00:34:43 -0400

    cleanup

--- build/build.ts
+++ build/build.ts
@@ -3,17 +3,8 @@ import { createBuilder } from "../lib/core/builder.ts";
 import { task } from "../lib/core/task.ts";
 import { DenoPermissions } from "../lib/deno/args.ts";
 import deno from "../lib/deno/deno.ts";
-import nodejs from "../lib/build-kits/nodejs/nodejs.ts";
-import runtime from "../lib/runtime/runtime.ts";
-
-const install = task('install')
-    .dependsOn(nodejs.setup('latest'))
-    .does(async ctx => {
-        await runtime.command({ cmd: ['npm', 'install', 'express'], logger: ctx?.logger });
-    });
 
 const test = task('test')
-    .dependsOn(install)
     .does(async ctx => {
         await deno.test({
             logger: ctx?.logger,
