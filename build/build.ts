0186db9d274e379b42371a1715a29a902463ff22
areller <areller.gm@gmail.com>
2020-10-12 20:32:19 -0400

    run prettier in build

--- build/build.ts
+++ build/build.ts
@@ -1,18 +1,29 @@
 import { createGitHubActions } from "../lib/ci/gh-actions/mod.ts";
 import { createBuilder, task, deno, DenoPermissions, runtime } from "../lib/mod.ts";
+import { nodejs } from "../lib/build-kits/nodejs/mod.ts";
+
+const nodejsSetup = nodejs.setup('latest');
+
+const npmInstall = task('npm install')
+    .dependsOn(nodejsSetup)
+    .does(async ctx => {
+        await runtime.command({
+            cmd: ['npm', 'install'],
+            logger: ctx?.logger
+        });
+    });
 
 const checkFormat = task('check format')
+    .dependsOn(nodejsSetup)
+    .dependsOn(npmInstall)
     .does(async ctx => {
-        //TODO: add deno command for that in lib
         await runtime.command({
-            cmd: ['deno', 'fmt', '--check'],
-            throwOnFailure: true,
+            cmd: ['npm', 'run', 'check-format'],
             logger: ctx?.logger
         });
     });
 
 const test = task('test')
-    .dependsOn(checkFormat)
     .does(async ctx => {
         await deno.test({
             logger: ctx?.logger,
@@ -23,7 +34,7 @@ const test = task('test')
 
 createBuilder({
     name: 'denogent-build',
-    targetTasks: test,
+    targetTasks: [checkFormat, test],
     ciIntegrations: [
         createGitHubActions({
             image: 'ubuntu-latest',
