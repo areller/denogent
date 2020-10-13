b0dd5d851933902e4f283f35b307b7a28a67f30a
areller <areller.gm@gmail.com>
2020-10-12 22:45:58 -0400

    nest publish

--- build/build.ts
+++ build/build.ts
@@ -1,5 +1,5 @@
 import { createGitHubActions } from '../lib/ci/gh-actions/mod.ts';
-import { createBuilder, task, deno, DenoPermissions, runtime } from '../lib/mod.ts';
+import { createBuilder, task, deno, DenoPermissions, runtime, git, fs } from '../lib/mod.ts';
 import { nodejs } from '../lib/build-kits/nodejs/mod.ts';
 
 const nodejsSetup = nodejs.setup('latest');
@@ -30,6 +30,39 @@ const test = task('test').does(async ctx => {
     flags: ['--unstable'],
   });
 });
+
+const replaceVersion = task('replace version')
+  .dependsOn(test)
+  .does(async ctx => {
+    const version = await git.describe({ logger: false }) ?? '';
+
+    ctx?.logger.debug('version: ' + version);
+
+    const versionFile = (await fs.getFile(['cli', 'version.ts']))!;
+    let contents = await versionFile.getContentsString();
+    contents = contents.replaceAll('{{VERSION}}', (await git.describe({ logger: false })) ?? '');
+    await versionFile.override(contents);
+  });
+
+const publish = task('publish')
+  .dependsOn(runtime.secret('NEST_API_KEY'))
+  .dependsOn(replaceVersion)
+  .does(async ctx => {
+    await runtime.command({
+      cmd: ['deno', 'instasll', '-A', '-f', '--unstable', '-n', 'eggs', 'https://x.nest.land/eggs@0.2.3/mod.ts'],
+      logger: ctx?.logger
+    });
+
+    await runtime.command({
+      cmd: ['eggs', 'link', runtime.argValue('NEST_API_KEY')],
+      logger: ctx?.logger
+    });
+
+    await runtime.command({
+      cmd: ['eggs', 'publish'],
+      logger: ctx?.logger
+    });
+  });
 
 createBuilder({
   name: 'denogent-build',
