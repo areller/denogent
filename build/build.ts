8dd9c16461edbb702ce26273f86a8e1d1b949bb2
areller <areller.gm@gmail.com>
2020-10-13 22:09:28 -0400

    CLI E2E tests (#15)
    * basic e2e tests and running during build
    
    * [gh-actions] do not log for tasks that don't perform anything
    
    * e2e for all current commands
    
    * formatting
    
    * fix import

--- build/build.ts
+++ build/build.ts
@@ -16,6 +16,7 @@ const npmInstall = task("npm install")
 const checkFormat = task("check format")
   .dependsOn(nodejsSetup)
   .dependsOn(npmInstall)
+  .when((ctx) => ctx?.ci !== undefined)
   .does(async (ctx) => {
     await runtime.command({
       cmd: ["npm", "run", "check-format"],
@@ -23,13 +24,27 @@ const checkFormat = task("check format")
     });
   });
 
-const test = task("test").does(async (ctx) => {
+const unitTests = task("unit tests").does(async (ctx) => {
   await deno.test({
     logger: ctx?.logger,
     permissions: DenoPermissions.All,
     flags: ["--unstable"],
+    filter: "[unit]",
   });
 });
+
+const e2eTests = task("e2e tests")
+  .dependsOn(unitTests)
+  .does(async (ctx) => {
+    await deno.test({
+      logger: ctx?.logger,
+      permissions: DenoPermissions.All,
+      flags: ["--unstable"],
+      filter: "[e2e]",
+    });
+  });
+
+const test = task("test").dependsOn([unitTests, e2eTests]);
 
 createBuilder({
   name: "denogent-build",
---
+++ cli/e2e/create.test.ts
@@ -0,0 +1,27 @@
+import { stdFs, stdPath } from "../../deps.ts";
+import { runCommand } from "../../internal/helpers/cmd.ts";
+import { emptyTempDir } from "../../internal/testing/helpers.ts";
+import { describeE2E } from "../../internal/testing/test.ts";
+import { assertEquals } from "../../tests_deps.ts";
+
+const denogent = ["deno", "run", "-A", "--unstable", stdPath.join(Deno.cwd(), "denogent.ts")];
+
+describeE2E("create.test.ts", (t) => {
+  [undefined, stdPath.join("build", "build.ts"), stdPath.join("build", "build2.ts")].forEach((filePath) => {
+    t.test(`'denogent create' should create build file (--file = ${filePath})`, async () => {
+      await emptyTempDir(async (temp) => {
+        const [success, _] = await runCommand(
+          [...denogent, "create", ...(filePath !== undefined ? ["--file", filePath] : [])],
+          undefined,
+          temp,
+          false,
+        );
+        assertEquals(success, true);
+        assertEquals(
+          await stdFs.exists(stdPath.join(temp, filePath === undefined ? stdPath.join("build", "build.ts") : filePath)),
+          true,
+        );
+      });
+    });
+  });
+});
