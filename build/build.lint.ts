56d696b9de9fc2484d8d19b9e2d16f98175f6cda
areller <areller.gm@gmail.com>
2020-10-16 18:57:09 -0400

    separate into multiple build files

---
+++ build/build.lint.ts
@@ -0,0 +1,26 @@
+import { nodejs, runtime, task } from "../mod.ts";
+
+const nodejsSetup = nodejs.setup("latest");
+
+const npmInstall = task("npm install")
+  .dependsOn(nodejsSetup)
+  .does(async (ctx) => {
+    await runtime.command({
+      cmd: ["npm", "install"],
+      logger: ctx?.logger,
+    });
+  });
+
+const lint = task("lint")
+  .dependsOn(nodejsSetup)
+  .dependsOn(npmInstall)
+  .when((ctx) => ctx?.ci !== undefined)
+  .does(async (ctx) => {
+    await runtime.command({
+      cmd: ["npm", "run", "lint"],
+      logger: ctx?.logger,
+    });
+  });
+
+const tasks = [npmInstall, lint];
+export { tasks };
