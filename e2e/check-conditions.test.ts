6fd58f755e58220f63db04e8d7c586924a4b7017
areller <areller.gm@gmail.com>
2020-10-17 23:31:39 -0400

    fix dep

--- e2e/check-conditions.test.ts
+++ e2e/check-conditions.test.ts
@@ -1,7 +1,7 @@
-import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 import { stdPath } from "../deps.ts";
 import { runCommand } from "../src/internal/helpers/cmd.ts";
 import { describeE2E } from "../src/internal/testing/test.ts";
+import { assertEquals } from "../tests_deps.ts";
 import { createBuildInTempDir } from "./common.ts";
 
 const denogent = ["deno", "run", "-A", "--no-check", "--unstable", stdPath.join(Deno.cwd(), "denogent.ts")];
